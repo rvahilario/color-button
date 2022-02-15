07f066afb09da8549064c26ed6f1e7babbcf3966
Renan Hilario <rvahilario@gmail.com>
2022-02-15 20:59:15 -0300

    test: color button test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('button has correct initial color', () => {});
+test('button has correct initial color', () => {
+	render(<App />);
+	const colorButton = screen.getByRole('button', { name: 'Change to blue' });
 
-test('button has correct initial text', () => {});
+	expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+});
 
 test('button turns blue when clicked', () => {});
