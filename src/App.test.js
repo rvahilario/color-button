4074f7946c2e8d6a78474cae28a9a92a2469baf9
Renan Hilario <rvahilario@gmail.com>
2022-02-15 20:47:46 -0300

    test: setting initial tests
    write initial tests that describe functionalities that we want

--- src/App.test.js
+++ src/App.test.js
@@ -2,3 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {});
+
+test('button has correct initial text', () => {});
+
+test('button turns blue when clicked', () => {});
