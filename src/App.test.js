6bc492e1945e7678227ffd398ec2d061a9e83bf8
Renan Hilario <rvahilario@gmail.com>
2022-02-15 21:03:56 -0300

    test: change literal string to regex case insensitive

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('button has correct initial color', () => {
 	render(<App />);
-	const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+	const colorButton = screen.getByRole('button', { name: /change to blue/i });
 
 	expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
 });
